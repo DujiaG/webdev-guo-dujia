a19b75b31dab4a007ac611e1b4348142408b281e
Dujia Guo <dujia.guo2015@gmail.com>
2017-11-27 15:17:53 -0500

    fixed heroku build

--- server/assignment/model/models.server.js
+++ server/assignment/model/models.server.js
@@ -6,9 +6,10 @@ module.exports = db;
 */
 var mongoose = require('mongoose');
 var connectionString = 'mongodb://localhost/cs5610';
- if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-   connectionString = 'mongodb://DujiaG:Gdjia.1993a@ds129434.mlab.com:29434/heroku_bk6cmpn8';
- }
+// connectionString = 'mongodb://DujiaG:Gdjia.1993a@ds129434.mlab.com:29434/heroku_bk6cmpn8';
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+     connectionString = 'mongodb://DujiaG:Gdjia.1993a@ds129434.mlab.com:29434/heroku_bk6cmpn8';
+}
 //   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
 //   var password = process.env.MLAB_PASSWORD_WEBDEV;
 //   connectionString = 'mongodb://' + username + ':' + password;
