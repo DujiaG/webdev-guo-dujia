37f8f1c2639d001576ff30992975ff1437308532
Dujia Guo <dujia.guo2015@gmail.com>
2017-11-27 12:08:20 -0500

    fix mongodb

--- server/assignment/model/models.server.js
+++ server/assignment/model/models.server.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/cs5610', { useMongoClient: true});
 module.exports = db;
 */
+var mongoose = require("mongoose");
 var connectionString = 'mongodb://localhost/cs5610';
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
@@ -11,6 +12,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   connectionString = 'mongodb://' + username + ':' + password;
   connectionString += 'ds129434.mlab.com:29434/heroku_bk6cmpn8';
 }
-var mongoose = require("mongoose");
+
+var connectionString = 'mongodb://DujiaG:Gdjia.1993a@ds129434.mlab.com:29434/heroku_bk6cmpn8';
 var db = mongoose.connect(connectionString, { useMongoClient: true});
 module.exports = db;
