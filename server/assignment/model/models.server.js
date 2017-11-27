6307b0b7216c48fbf11ad508d5b67beecb8460cc
Dujia Guo <dujia.guo2015@gmail.com>
2017-11-27 12:11:04 -0500

    mongodb

--- server/assignment/model/models.server.js
+++ server/assignment/model/models.server.js
@@ -5,13 +5,13 @@ var db = mongoose.connect('mongodb://localhost/cs5610', { useMongoClient: true})
 module.exports = db;
 */
 var mongoose = require("mongoose");
-var connectionString = 'mongodb://localhost/cs5610';
+/*var connectionString = 'mongodb://localhost/cs5610';
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
   connectionString += 'ds129434.mlab.com:29434/heroku_bk6cmpn8';
-}
+}*/
 
 var connectionString = 'mongodb://DujiaG:Gdjia.1993a@ds129434.mlab.com:29434/heroku_bk6cmpn8';
 var db = mongoose.connect(connectionString, { useMongoClient: true});
