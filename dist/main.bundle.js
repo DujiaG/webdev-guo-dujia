35f1fda739c0e685ac7ee75c138cb0b792f04edb
Dujia Guo <dujia.guo2015@gmail.com>
2017-12-08 01:53:16 -0500

    fixed baseUrl

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -3444,7 +3444,7 @@ var _a;
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
 var environment = {
     production: true,
-    baseUrl: 'http://localhost:3100',
+    baseUrl: '',
     FACEBOOK_CLIENT_ID: '944658295684913',
     FACEBOOK_CLIENT_SECRET: '8f83840a26d92dd8a17999aa6f7ae1a4',
     FACEBOOK_CALLBACK_URL: 'http://localhost:3100/auth/facebook/callback'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl : 'http://localhost:3100',
+  baseUrl : '',
   FACEBOOK_CLIENT_ID: '944658295684913',
   FACEBOOK_CLIENT_SECRET: '8f83840a26d92dd8a17999aa6f7ae1a4',
   FACEBOOK_CALLBACK_URL: 'http://localhost:3100/auth/facebook/callback'
